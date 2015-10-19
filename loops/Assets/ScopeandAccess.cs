8708dbf975075415a3f7cd4b459297ae6068308c
unknown <10628452@CS514-12.ad.uvu.edu>
2015-10-19 09:21:46 -0600

    Scope and Access Modifiers

---
+++ loops/Assets/ScopeandAccess.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeandAccess : MonoBehaviour 
+{
+	public int alpha = 5;
+	
+	private int beta = 0;
+	private int gamma = 5;
+	
+	private AnotherClass myOtherClass;
+
+	// Use this for initialization
+	void Start () 
+	{
+		alpha = 29;
+		
+		myOtherClass = new AnotherClass();
+		myOtherClass.FruitMachine(alpha, myOtherClass.apples);
+		
+	}
+	
+
+	void Example(int pens, int crayons)
+	{
+		int answer;
+		answer = pens * crayons * alpha;
+		Debug.Log(answer);
+	}
+
+	void Update () 
+	{
+		Debug.Log("Alpha is set to:" + alpha);
+	}
+}
