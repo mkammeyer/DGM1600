3131702b097e474eefbc6b770e919d396acbc10a
unknown <10628452@CS514-12.ad.uvu.edu>
2015-12-07 09:09:13 -0700

    Duck Creation and Destroy assignment

---
+++ DuckHunt/Assets/Scripts/DuckSpawn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckSpawn : MonoBehaviour 
+{
+	public GameObject Duck;
+
+	
+	public void SpawnDuck()
+	{
+	// Instantiate here with Duck and transform.position and Quaterion.identiy
+		Instantiate(Duck, transform.position, Quaternion.identity);
+	
+	}
+}
