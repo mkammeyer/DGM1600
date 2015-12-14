7042788e816c95681ee1cced285bec47c365c388
unknown <10628452@CS514-12.ad.uvu.edu>
2015-12-14 08:52:43 -0700

    DuckHunt

---
+++ DuckHunt/Assets/Scripts/DirectionChanger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionChanger : MonoBehaviour 
+{
+	public enum DirChanger{Horizontal,Vertical};
+	public DirChanger changer;
+	
+	private DuckMovement duckMovement;
+	
+	void OnCollisionEnter(Collision hit)
+	{
+		if(hit.transform.tag == "Duck")
+		{
+			//create duckmovement varible. Assign using getComponent of hit
+			duckMovement = GetComponent<DuckMovement>();
+			
+			if(changer == DirChanger.Horizontal)
+			{
+				// duckmovement varible and call the directionchanger function. new vector3(-1,1,0)
+				duckMovement.DirectionChanger(new Vector3(-1,1,0));
+			}
+			if(changer == DirChanger.Vertical)
+			{
+				// duckmovement varible and call the directionchanger function. new vector3(1,-1,0)
+				duckMovement.DirectionChanger(new Vector3(1,-1,0));
+			}
+		}
+	}
+
+
+}
---
+++ DuckHunt/Assets/Scripts/DuckMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuckMovement : MonoBehaviour 
+{
+	public float speed;
+	public Vector3 direction;
+	
+	void Start()
+	{
+		RandomDirection();
+	}
+	
+	void Update () 
+	{
+		//move the duck by changing the position of the transform. Times.direction with speed
+		transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+	
+	}
+	
+	public void RandomDirection ()
+	{
+		direction = new Vector3(Random.Range(-1f, 1f), Random.Range(.2f, 1f), 0);
+	}
+	
+	public void DirectionChanger(Vector3 _dir)
+	{
+		direction = new Vector3(direction.x * _dir.x, direction.y * _dir.y, 0);
+	}
+}
